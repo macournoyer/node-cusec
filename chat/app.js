06195c180be9e75ece6231be4a6477294fa0a723
macournoyer <macournoyer@gmail.com>
2010-01-20 00:00:35 -0500

    Make express code more compliant.

--- chat/app.js
+++ chat/app.js
@@ -1,25 +1,25 @@
 require.paths.unshift('/Users/ma/projects/express/lib');
-require('express')
-require('express/plugins')
+require('express');
+require('express/plugins');
 
 configure(function(){
-  use(MethodOverride)
-  use(ContentLength)
-  use(CommonLogger)
-  set('root', dirname(__filename))
-})
+  use(MethodOverride);
+  use(ContentLength);
+  use(CommonLogger);
+  set('root', dirname(__filename));
+});
 
 get('/', function(){
-  this.contentType('html')
-  return '<h1>Hello World<h1>'
-})
+  this.contentType('html');
+  return '<h1>Hello World<h1>';
+});
 
 get('/*.css', function(file){
-  this.render(file + '.sass.css', { layout: false })
-})
+  this.render(file + '.sass.css', { layout: false });
+});
 
 get('/*.js', function(file){
-  this.sendfile(dirname(__filename) + '/public/' + file + '.js')
-})
+  this.sendfile(dirname(__filename) + '/public/' + file + '.js');
+});
 
-run()
+run();
