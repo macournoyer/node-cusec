1eff533d664315970e087ca149621fd80f055e65
macournoyer <macournoyer@gmail.com>
2010-01-20 20:44:27 -0500

    Add some comments in code

--- chat/app.js
+++ chat/app.js
@@ -8,16 +8,19 @@ configure(function(){
   set('root', dirname(__filename));
 });
 
+
 get('/', function(){
   return 'Hello World';
 });
 
+
+// Serve assets
 get('/*.css', function(file){
   this.render(file + '.sass.css', { layout: false });
 });
-
 get('/*.js', function(file){
   this.sendfile(dirname(__filename) + '/public/' + file + '.js');
 });
 
+// Run the app
 run();
